db6a46951cd2d252a64dcc02bacb354041dce4c3
Henrik Thorburn <boaworm@gmail.com>
2026-05-09 10:13:09 -0500

    Removing paywall content from expressen

--- anti-paywall/sites/expressen.js
+++ anti-paywall/sites/expressen.js
@@ -1,9 +1,17 @@
 initSite({
   overlaySelectors: [
+    // Existing paywall overlay selectors
     '#tapet-paywall',
     '.bn-tapet',
     '.tapet-paywall',
     '.paywall-manager__fallback',
+    
+    // Premium article links - remove the link element itself
+    'a.premium-link',
+    'a.slider__item.premium-link',
+    
+    // Premium icons - remove the icon elements
+    '.premium-icon',
   ],
   fixBodyScroll: true,
 });
